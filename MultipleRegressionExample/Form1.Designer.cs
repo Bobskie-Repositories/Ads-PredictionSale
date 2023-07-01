89f7d7282230fed49b60f63d50a9783f4c4573e6
Bob Kyle L. Rosales <bobkyle.rosales@cit.edu>
2023-07-01 22:48:02 +0800

    Intiial Commit

---
+++ MultipleRegressionExample/Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace Bobskie_Regression
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.inputx1 = new System.Windows.Forms.TextBox();
+            this.inputx2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.regressionEquationOutput = new System.Windows.Forms.Label();
+            this.regressionOutput = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.reslabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(69, 144);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(218, 38);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Show Equation";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // inputx1
+            // 
+            this.inputx1.Location = new System.Drawing.Point(279, 261);
+            this.inputx1.Name = "inputx1";
+            this.inputx1.Size = new System.Drawing.Size(127, 22);
+            this.inputx1.TabIndex = 3;
+            // 
+            // inputx2
+            // 
+            this.inputx2.Location = new System.Drawing.Point(279, 311);
+            this.inputx2.Name = "inputx2";
+            this.inputx2.Size = new System.Drawing.Size(127, 22);
+            this.inputx2.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(76, 364);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(211, 41);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Calculate";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(225, 267);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 16);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "x1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(220, 314);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(20, 16);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "x2";
+            // 
+            // regressionEquationOutput
+            // 
+            this.regressionEquationOutput.AutoSize = true;
+            this.regressionEquationOutput.Location = new System.Drawing.Point(83, 71);
+            this.regressionEquationOutput.Name = "regressionEquationOutput";
+            this.regressionEquationOutput.Size = new System.Drawing.Size(0, 16);
+            this.regressionEquationOutput.TabIndex = 10;
+            // 
+            // regressionOutput
+            // 
+            this.regressionOutput.AutoSize = true;
+            this.regressionOutput.Location = new System.Drawing.Point(436, 74);
+            this.regressionOutput.Name = "regressionOutput";
+            this.regressionOutput.Size = new System.Drawing.Size(61, 16);
+            this.regressionOutput.TabIndex = 11;
+            this.regressionOutput.Text = "Intercept:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(436, 113);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(99, 16);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Operation Cost:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(436, 155);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(94, 16);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Ad Frequency:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(584, 74);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(10, 16);
+            this.label6.TabIndex = 14;
+            this.label6.Text = " ";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(584, 113);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(10, 16);
+            this.label7.TabIndex = 15;
+            this.label7.Text = " ";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(584, 155);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(10, 16);
+            this.label8.TabIndex = 16;
+            this.label8.Text = " ";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(542, 29);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(116, 22);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "Coefficients";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(83, 222);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(160, 22);
+            this.label10.TabIndex = 18;
+            this.label10.Text = "Predict the sales";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(120, 314);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(94, 16);
+            this.label11.TabIndex = 20;
+            this.label11.Text = "Ad Frequency:";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(120, 267);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(99, 16);
+            this.label12.TabIndex = 19;
+            this.label12.Text = "Operation Cost:";
+            // 
+            // reslabel
+            // 
+            this.reslabel.AutoSize = true;
+            this.reslabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.reslabel.ForeColor = System.Drawing.Color.Red;
+            this.reslabel.Location = new System.Drawing.Point(542, 297);
+            this.reslabel.Name = "reslabel";
+            this.reslabel.Size = new System.Drawing.Size(0, 22);
+            this.reslabel.TabIndex = 21;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(507, 264);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(181, 20);
+            this.label1.TabIndex = 22;
+            this.label1.Text = "The Predicted Sale: ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.reslabel);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.regressionOutput);
+            this.Controls.Add(this.regressionEquationOutput);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.inputx2);
+            this.Controls.Add(this.inputx1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Bobskie Multiple Regression Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox inputx1;
+        private System.Windows.Forms.TextBox inputx2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label regressionEquationOutput;
+        private System.Windows.Forms.Label regressionOutput;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label reslabel;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ MultipleRegressionExample/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Windows.Forms;
+using Accord.Statistics.Models.Regression.Linear;
+
+namespace Bobskie_Regression
+{
+    public partial class Form1 : Form
+    {
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private (double[][], double[]) GetDatasets()
+        {
+            double[][] inputs = {
+                new double[] {471.4, 8},
+                new double[] {514.3, 20},
+                new double[] {612.9, 22},
+                new double[] {642.9, 24},
+                new double[] {651.4, 30},
+                new double[] {694.3, 36},
+                new double[] {771.4, 36},
+                new double[] {780, 38},
+                new double[] {771.4, 36},
+                new double[] {750, 38},
+                new double[] {835.7, 50},
+                new double[] {840, 54},
+                new double[] {784.3 ,58},
+                new double[] {814.3, 50},
+                new double[] {642.9, 48},
+            };
+            double[] outputs = {
+                360,
+                720,
+                1140,
+                1500,
+                1800,
+                2160,
+                2280,
+                2580,
+                2640,
+                2940,
+                3000,
+                3120,
+                3240,
+                2160,
+                2460
+            };
+
+            return (inputs, outputs);
+        }
+
+        private OrdinaryLeastSquares GetRegressionModel(bool useIntercept)
+        {
+            return new OrdinaryLeastSquares()
+            {
+                UseIntercept = useIntercept
+            };
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            (double[][] inputs, double[] outputs) = GetDatasets();
+
+            // Create an Ordinary Least Squares (OLS) object to fit the regression
+            var ols = GetRegressionModel(useIntercept: true);
+
+            // Learn the multiple regression model using the OLS algorithm
+            var regression = ols.Learn(inputs, outputs);
+
+            // Get the intercept and coefficients (weights) of the multiple regression model
+            var intercept = regression.Intercept;
+            var coefficients = regression.Weights;
+
+            // Build the regression equation string
+            string regressionEquation = "y' = " + intercept.ToString("N2");
+            for (int i = 0; i < coefficients.Length; i++)
+            {
+                regressionEquation += $" + {coefficients[i]:N2} *x{i + 1}";
+            }
+
+            // Output the regression equation to the textbox
+            regressionEquationOutput.Text = regressionEquation;
+
+            // Print the intercept and coefficients to the console
+            //textBox1.Text = string.Format("Intercept: {0}\r\nCoefficients: {1}",
+            //                    intercept, string.Join(", ", coefficients));
+            label6.Text = ""+intercept;
+            label7.Text = ""+coefficients[0];
+            label8.Text = "" + coefficients[1];
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            (double[][] inputs, double[] outputs) = GetDatasets();
+
+            // Create an Ordinary Least Squares (OLS) object to fit the regression
+            var ols = GetRegressionModel(useIntercept: true);
+
+            // Learn the multiple regression model using the OLS algorithm
+            var regression = ols.Learn(inputs, outputs);
+
+            var intercept = regression.Intercept;
+            var coefficients = regression.Weights;
+
+            /// Read the values of x1 and x2 from the TextBox controls
+            if (!double.TryParse(inputx1.Text, out double x1) || !double.TryParse(inputx2.Text, out double x2))
+            {
+                MessageBox.Show("Invalid input values!");
+                return;
+            }
+
+            // Calculate the value of y for the given values of x1 and x2
+            double y = intercept + (coefficients[0] * x1) + (coefficients[1] * x2);
+
+            // Display the result of the regression in the Label control
+            reslabel.Text = "y = " + y.ToString("N2");
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
